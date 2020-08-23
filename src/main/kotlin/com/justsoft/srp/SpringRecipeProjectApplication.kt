fa885acd1afdc3904503b119c75ff95e8037dc73
Max <rudko.max10@gmail.com>
2020-08-23 21:35:41 +0300

    Initial commit

---
+++ src/main/kotlin/com/justsoft/srp/SpringRecipeProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.justsoft.srp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringRecipeProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringRecipeProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/justsoft/srp/SpringRecipeProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.justsoft.srp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringRecipeProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
