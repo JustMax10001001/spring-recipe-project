4d5b89ddc861abe2869f633aedad522ed5e3d383
Max <rudko.max10@gmail.com>
2020-09-28 19:34:46 +0300

    Annotate IngredientMeasurementUnitRepository.kt as a repository

--- src/main/kotlin/com/justsoft/srp/repostitory/IngredientMeasurementUnitRepository.kt
+++ src/main/kotlin/com/justsoft/srp/repostitory/IngredientMeasurementUnitRepository.kt
@@ -2,8 +2,10 @@ package com.justsoft.srp.repostitory
 
 import com.justsoft.srp.model.IngredientMeasurementUnit
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 import java.util.*
 
+@Repository
 interface IngredientMeasurementUnitRepository: CrudRepository<IngredientMeasurementUnit, Long> {
 
     fun findByUnitName(unitName: String): IngredientMeasurementUnit?
